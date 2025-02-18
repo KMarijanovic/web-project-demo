30199719b33c525e6075ae57e9bb4c76d4f8f597
KMarijanovic <kristina.tina.marijanovic@gmail.com>
2025-02-18 14:08:01 +0100

    add data

---
+++ src/data/data.js
@@ -0,0 +1,324 @@
+// slider imgs
+import slideOne from "../images/slider/indoor-slide-one.jpg"
+import slideTwo from "../images/slider/pots-slide-two.jpg"
+import slideThree from "../images/slider/outdoor-slide-three.jpg"
+
+// arrivals imgs
+import newArrivalOne from "../images/arrivals/arriwals-one.jpg"
+import newArrivalTwo from "../images/arrivals/arrivals-two.jpg"
+import newArrivalThree from "../images/arrivals/arrivals-three.jpg"
+import newArrivalFour from "../images/arrivals/arrivals-four.jpg"
+import newArrivalFive from "../images/arrivals/arrivals-five.jpg"
+import newArrivalSix from "../images/arrivals/arrivals-six.jpg"
+import newArrivalSeven from "../images/arrivals/arrivals-seven.jpg"
+import newArrivalEight from "../images/arrivals/arrivals-eight.jpg"
+
+// news imgs
+import jungleNewsPic from "../images/news/news.jpg"
+
+// indoors imgs
+import aglaonema from "../images/indoors/air-purifying/aglaonema.jpg"
+import anthurium from "../images/indoors/air-purifying/anthurium.jpg"
+import bambooPalm from "../images/indoors/air-purifying/bamboo-palm.jpg"
+import gerberaDaisy from "../images/indoors/air-purifying/gerbera-daisy.jpg"
+import peaceLily from "../images/indoors/air-purifying/peace-lily.jpg"
+
+import aloeVera from "../images/indoors/easy-care/aloe-vera.jpg"
+import asparagusFern from "../images/indoors/easy-care/asparagus-fern.jpg"
+import dracaena from "../images/indoors/easy-care/dracaena.jpg"
+import monstera from "../images/indoors/easy-care/monstera.jpg"
+import rubberPlant from "../images/indoors/easy-care/rubber-plant.jpg"
+
+import bostonFern from "../images/indoors/pet-friendly/boston-fern.jpg"
+import calathea from "../images/indoors/pet-friendly/calathea.jpg"
+import parlorPalm from "../images/indoors/pet-friendly/parlor-palm.jpg"
+import ponytailPalm from "../images/indoors/pet-friendly/ponytail-palm.jpg"
+import prayerPlant from "../images/indoors/pet-friendly/prayer-plant.jpg"
+
+import philodendron from "../images/indoors/shade-tolerant/philodendron.jpg"
+import pothos from "../images/indoors/shade-tolerant/pothos.jpg"
+import sanseveria from "../images/indoors/shade-tolerant/sanseveria.jpg"
+import spiderPlant from "../images/indoors/shade-tolerant/spider-plant.jpg"
+import zamioculcasZamiifolia from "../images/indoors/shade-tolerant/zamioculcas-zamiifolia.jpg"
+
+
+export const sliderItems = [
+    {
+        id: 1,
+        title: "INDOOR PLANTS",
+        desc: "Discover our indoor plants collection that will transform your home.",
+        img: slideOne,
+    },
+    {
+        id: 2,
+        title: "POTS & CARE",
+        desc: "See our tips & tricks about pots and care for every plant lover.",
+        img: slideTwo,
+    },
+    {
+        id: 3,
+        title: "OUTDOOR PLANTS",
+        desc: "Check out our outdoor plants offers for your garden.",
+        img: slideThree,
+    }
+]
+
+export const jungleNews = [
+    {
+        id: 1,
+        title: "NEWS ONE",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate beatae impedit labore? Consectetur facilis quas nemo cumque voluptatibus error, nihil inventore tempore esse sint exercitationem quasi voluptate? Sed, ad quidem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed praesentium deleniti, temporibus recusandae, ipsum delectus cum sint reiciendis impedit, rerum nobis animi dolorem voluptatibus excepturi molestiae sequi minus quisquam et. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptas dolores quod cum impedit ex aliquid, iure fugit tenetur eaque quasi dolore reprehenderit nam soluta unde non ut ipsam doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Non hic voluptate nulla quas, cum veritatis reprehenderit, nisi iusto harum, adipisci sed expedita! Labore error repellat porro magni expedita maiores sequi! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel excepturi molestias autem obcaecati placeat minima, esse nesciunt optio itaque nam blanditiis consequuntur labore consequatur aperiam quam nostrum. In, eveniet soluta.",
+        img: jungleNewsPic,
+    },
+    {
+        id: 2,
+        title: "NEWS TWO",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate beatae impedit labore? Consectetur facilis quas nemo cumque voluptatibus error, nihil inventore tempore esse sint exercitationem quasi voluptate? Sed, ad quidem!",
+        img: jungleNewsPic,
+    },
+    {
+        id: 3,
+        title: "NEWS THREE",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate beatae impedit labore? Consectetur facilis quas nemo cumque voluptatibus error, nihil inventore tempore esse sint exercitationem quasi voluptate? Sed, ad quidem!",
+        img: jungleNewsPic,
+    },
+    {
+        id: 4,
+        title: "NEWS FOUR",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate beatae impedit labore? Consectetur facilis quas nemo cumque voluptatibus error, nihil inventore tempore esse sint exercitationem quasi voluptate? Sed, ad quidem!",
+        img: jungleNewsPic,
+    },
+    {
+        id: 5,
+        title: "NEWS FIVE",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate beatae impedit labore? Consectetur facilis quas nemo cumque voluptatibus error, nihil inventore tempore esse sint exercitationem quasi voluptate? Sed, ad quidem!",
+        img: jungleNewsPic,
+    }
+]
+
+export const allindoors = [
+    {
+        id: 1,
+        title: "philodendron",
+        desc: "shade tolerant",
+        shadeTolerant: "yes",
+        img: philodendron,
+        price: "3"
+    },
+    {
+        id: 2,
+        title: "sanseveria",
+        desc: "shade tolerant",
+        shadeTolerant: "yes",
+        img: sanseveria,
+        price: "3"
+    },
+    {
+        id: 3,
+        title: "pothos",
+        desc: "shade tolerant",
+        shadeTolerant: "yes",
+        img: pothos,
+        price: "3"
+    },
+    {
+        id: 4,
+        title: "spider plant",
+        desc: "shade tolerant",
+        shadeTolerant: "yes",
+        img: spiderPlant,
+        price: "3"
+    },
+    {
+        id: 5,
+        title: "zamioculcas zamiifolia",
+        desc: "shade tolerant",
+        shadeTolerant: "yes",
+        img: zamioculcasZamiifolia,
+        price: "3"
+    },
+    {
+        id: 6,
+        title: "aloe vera",
+        desc: "easy care",
+        easyCare: "yes",
+        img: aloeVera,
+        price: "5"
+    },
+    {
+        id: 7,
+        title: "dracaena",
+        desc: "easy care",
+        easyCare: "yes",
+        img: dracaena,
+        price: "5"
+    },
+    {
+        id: 8,
+        title: "monstera",
+        desc: "easy care",
+        easyCare: "yes",
+        img: monstera,
+        price: "5"
+    },
+    {
+        id: 9,
+        title: "asparagus fern",
+        desc: "easy care",
+        easyCare: "yes",
+        img: asparagusFern,
+        price: "5"
+    },
+    {
+        id: 10,
+        title: "rubber plant",
+        desc: "easy care",
+        easyCare: "yes",
+        img: rubberPlant,
+        price: "5"
+    },
+    {
+        id: 11,
+        title: "boston fern",
+        desc: "pet friendly",
+        petFriendly: "yes",
+        img: bostonFern,
+        price: "5"
+    },
+    {
+        id: 12,
+        title: "prayer fern",
+        desc: "pet friendly",
+        petFriendly: "yes",
+        img: prayerPlant,
+        price: "5"
+    },
+    {
+        id: 13,
+        title: "ponytail fern",
+        desc: "pet friendly",
+        petFriendly: "yes",
+        img: ponytailPalm,
+        price: "5"
+    },
+    {
+        id: 14,
+        title: "parlor fern",
+        desc: "pet friendly",
+        petFriendly: "yes",
+        img: parlorPalm,
+        price: "5"
+    },
+    {
+        id: 15,
+        title: "calathea",
+        desc: "pet friendly",
+        petFriendly: "yes",
+        img: calathea,
+        price: "5"
+    },
+    {
+        id: 16,
+        title: "gerbera daisy",
+        desc: "air purifying",
+        airPurifying: "yes",
+        img: gerberaDaisy,
+        price: "7",
+    },
+    {
+        id: 17,
+        title: "aglaonema",
+        desc: "air purifying",
+        airPurifying: "yes",
+        img: aglaonema,
+        price: "7",
+    },
+    {
+        id: 18,
+        title: "anthurium",
+        desc: "air purifying",
+        airPurifying: "yes",
+        img: anthurium,
+        price: "7",
+    },
+    {
+        id: 19,
+        title: "bamboo palm",
+        desc: "air purifying",
+        airPurifying: "yes",
+        img: bambooPalm,
+        price: "7",
+    },
+    {
+        id: 20,
+        title: "peace lily",
+        desc: "air purifying",
+        airPurifying: "yes",
+        img: peaceLily,
+        price: "7",
+    },
+    {
+        id: 21,
+        title: "pilosocereus pachycladus",
+        desc: "easy care",
+        newArrival: "yes",
+        img: newArrivalOne,
+        price: "9"
+    },
+    {
+        id: 22,
+        title: "fasciated haworthia",
+        desc: "shade tolerant",
+        newArrival: "yes",
+        img: newArrivalTwo,
+        price: "8.50"
+    },
+    {
+        id: 23,
+        title: "heartleaf philodendron",
+        desc: "shade tolerant",
+        newArrival: "yes",
+        img: newArrivalThree,
+        price: "5"
+    },
+    {
+        id: 24,
+        title: "ficus microcarpa",
+        desc: "easy care",
+        newArrival: "yes",
+        img: newArrivalFour,
+        price: "7"
+    },
+    {
+        id: 25,
+        title: "zamioculcas zamiifolia",
+        desc: "shade tolerant",
+        newArrival: "yes",
+        img: newArrivalFive,
+        price: "6.50"
+    },
+    {
+        id: 26,
+        title: "gasteria armstrongii",
+        desc: "easy care",
+        newArrival: "yes",
+        img: newArrivalSix,
+        price: "9.50"
+    },
+    {
+        id: 27,
+        title: "eucalyptus cinerea",
+        desc: "air purifying",
+        newArrival: "yes",
+        img: newArrivalSeven,
+        price: "5"
+    },
+    {
+        id: 28,
+        title: "aloe vera",
+        desc: "air purifying",
+        newArrival: "yes",
+        img: newArrivalEight,
+        price: "4.50"
+    },
+]

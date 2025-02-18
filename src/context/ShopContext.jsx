758832beddc1aad59d5cfbdebd1e5de60174512c
KMarijanovic <kristina.tina.marijanovic@gmail.com>
2025-02-18 14:06:37 +0100

    add context

---
+++ src/context/ShopContext.jsx
@@ -0,0 +1,63 @@
+// defining states & logic in project
+
+import { createContext, useState } from "react"
+import { allindoors } from "../data/data"
+
+// keeps track of our states
+export const ShopContext = createContext(null)
+
+const getDefaultCart = () => {
+    let cart = {}
+    for (let i = 1; i < allindoors.length + 1; i++) {
+        cart[i] = 0;
+    }
+    return cart;
+}
+
+const ShopContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState(getDefaultCart())
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = allindoors.find((product) => product.id === Number(item))
+                totalAmount += cartItems[item] * itemInfo.price
+            }
+        }
+        return totalAmount
+    };
+
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    }
+
+    const updateCartItemCount = (newAmount, itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: newAmount }))
+    }
+
+    const deleteFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: 0 }))
+    }
+
+    // placing it all here to have an access to all of it
+    const contextValue = {
+        cartItems,
+        addToCart,
+        removeFromCart,
+        updateCartItemCount,
+        getTotalCartAmount,
+        deleteFromCart
+    }
+
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+export default ShopContextProvider
